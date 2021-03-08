bc739e85860be1901ae0aeae2a6c39a685850507
Robert Connot <rmconnot@umich.edu>
2021-03-08 17:13:14 -0500

    fixed urql Provider client

--- App.js
+++ App.js
@@ -1,21 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+	FlatList,
+	StyleSheet,
+	Text,
+	TextInput,
+	Touchable,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import { Provider, createClient, useQuery, useMutation } from "urql";
+
+const client = createClient({
+	url: "https://mechanicus--backend.herokuapp.com/graphql",
+});
+
+const getData = `
+query {
+	messages {
+		id
+		content
+	}
+}
+`;
+
+const Input = () => {
+	const updateMessage = `mutation ($input: String!) {
+		post(
+			content: $input
+		) {
+			id
+			content
+		}}`;
+	const [input, setInput] = useState("");
+	const [request, makeRequest] = useMutation(updateMessage);
+
+	const handleSubmission = () => {
+		makeRequest({ input }).then((result) => {
+			if (result.error) {
+				console.error("Oh no!", result.error);
+			}
+		});
+	};
+
+	return (
+		<View style={styles.requestView}>
+			<TextInput
+				style={styles.inputBox}
+				onChangeText={(text) => setInput(text)}
+			></TextInput>
+			<TouchableOpacity style={styles.button} onPress={handleSubmission}>
+				<Text>Submit content</Text>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+const Messages = () => {
+	const [result, reexecuteQuery] = useQuery({
+		query: getData,
+	});
+
+	const { data, fetching, error } = result;
+
+	if (fetching) return <Text>Loading...</Text>;
+	if (error) return <Text>Oh no... {error.message}</Text>;
+
+	return (
+		<FlatList
+			data={data.messages}
+			renderItem={({ item }) => (
+				<Text key={String(item.id)}>{item.content}</Text>
+			)}
+			style={styles.list}
+		/>
+	);
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<Provider value={client}>
+			<View style={styles.container}>
+				<Input />
+				<View style={styles.responseView}>
+					<Messages />
+				</View>
+				<StatusBar style="auto" />
+			</View>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	responseView: {
+		flex: 2,
+	},
+	requestView: {
+		flex: 1,
+		marginTop: 400,
+	},
+
+	button: {
+		borderStyle: "solid",
+		borderWidth: 2,
+		marginTop: 10,
+	},
+
+	inputBox: {
+		borderStyle: "solid",
+		borderWidth: 2,
+	},
 });
