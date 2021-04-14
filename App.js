cfa922bcfc7c1d86aec44bd9d37cc33301ef87af
Xiaoshan He <40234680+xiaoshanhe003@users.noreply.github.com>
2021-04-13 21:55:07 -0400

    Revert "Added common styles, added quote card"

--- App.js
+++ App.js
@@ -1,119 +1,119 @@
-import React from "react";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import {
-	ApolloClient,
-	InMemoryCache,
-	ApolloProvider,
-	split,
-	HttpLink,
-} from "@apollo/client";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { WebSocketLink } from "@apollo/client/link/ws";
+	FlatList,
+	StyleSheet,
+	Text,
+	TextInput,
+	Touchable,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import { Provider, createClient, useQuery, useMutation } from "urql";
 
-// SCREENS-LOG IN, Sign up
-import LoginScreen from "./js/screens/Login/LoginScreen";
-import { SignUpScreen } from "./js/screens/SignUp/SignUpScreen";
-
-// SCREENS-GET A QUOTE
-import QuoteVehicleScreen from "./js/screens/QuoteVehicle/QuoteVehicleScreen";
-import QuoteServiceScreen from "./js/screens/QuoteService/QuoteServiceScreen";
-import QuoteReviewScreen from "./js/screens/QuoteReview/QuoteReviewScreen";
-
-// SCREENS-TASK
-
-import { TaskListScreen } from "./js/screens/TaskList/TaskListScreen";
-import TaskDetailPastScreen from "./js/screens/TaskDetailPast/TaskDetailPastScreen";
-import TaskDetailPresentScreen from "./js/screens/TaskDetailPresent/TaskDetailPresentScreen";
-
-import AddVehicleVINScreen from "./js/screens/AddVehicleVIN/AddVehicleVINScreen";
-import AddVehicleManualScreen from "./js/screens/AddVehicleManual/AddVehicleManualScreen";
-// import { VehicleListScreen } from "./js/screens/VehicleList/VehicleListScreen";
-import { ScheduleScreen } from "./js/screens/Schedule/ScheduleScreen";
-
-// SCREENS-PROFILE
-import ProfileScreen from "./js/screens/Profile/ProfileScreen";
-
-// TEST MODULE
-import TEST from "./moduleTest";
-
-// =====
-// nav dependencies
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-// navigation
-const Stack = createStackNavigator();
-
-// connect to backend
-
-
-const IP = "192.168.0.5";
-// You will need to replace '192.168.1.126' with your IP address
-const httpLink = new HttpLink({
-
-	uri: `http://${IP}:4000/graphql`,
+const client = createClient({
+	url: "https://mechanicus--backend.herokuapp.com/graphql",
 });
 
-const wsLink = new WebSocketLink({
-	uri: `ws://${IP}:4000/subscriptions`,
-	options: {
-		reconnect: true,
-	},
-});
+const getData = `
+query {
+	messages {
+		id
+		content
+	}
+}
+`;
 
-const splitLink = split(
-	({ query }) => {
-		const definition = getMainDefinition(query);
-		return (
-			definition.kind === "OperationDefinition" &&
-			definition.operation === "subscription"
-		);
-	},
-	wsLink,
-	httpLink
-);
+const Input = () => {
+	const updateMessage = `mutation ($input: String!) {
+		post(
+			content: $input
+		) {
+			id
+			content
+		}}`;
+	const [input, setInput] = useState("");
+	const [request, makeRequest] = useMutation(updateMessage);
 
-const client = new ApolloClient({
-	link: splitLink,
-	cache: new InMemoryCache(),
-});
+	const handleSubmission = () => {
+		makeRequest({ input }).then((result) => {
+			if (result.error) {
+				console.error("Oh no!", result.error);
+			}
+		});
+	};
+
+	return (
+		<View style={styles.requestView}>
+			<TextInput
+				style={styles.inputBox}
+				onChangeText={(text) => setInput(text)}
+			></TextInput>
+			<TouchableOpacity style={styles.button} onPress={handleSubmission}>
+				<Text>Submit content</Text>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+const Messages = () => {
+	const [result, reexecuteQuery] = useQuery({
+		query: getData,
+	});
+
+	const { data, fetching, error } = result;
+
+	if (fetching) return <Text>Loading...</Text>;
+	if (error) return <Text>Oh no... {error.message}</Text>;
+
+	return (
+		<FlatList
+			data={data.messages}
+			renderItem={({ item }) => (
+				<Text key={String(item.id)}>{item.content}</Text>
+			)}
+			style={styles.list}
+		/>
+	);
+};
 
 export default function App() {
 	return (
-		<ApolloProvider client={client}>
-			<NavigationContainer>
-				<Stack.Navigator initialRouteName="QuoteVehicle">
-					<Stack.Screen name="LogIn" component={LoginScreen} />
-					<Stack.Screen name="SignUp" component={SignUpScreen} />
-					{/* <Stack.Screen name="Register" component={RegisterScreen} /> */}
-
-					<Stack.Screen name="TaskList" component={TaskListScreen} />
-					<Stack.Screen
-						name="TaskDetailPast"
-						component={TaskDetailPastScreen}
-					/>
-					<Stack.Screen
-						name="TaskDetailPresent"
-						component={TaskDetailPresentScreen}
-					/>
-
-					{/* <Stack.Screen name="VehicleList" component={VehicleListScreen} /> */}
-					<Stack.Screen name="AddVehicleVIN" component={AddVehicleVINScreen} />
-					<Stack.Screen
-						name="AddVehicleManual"
-						component={AddVehicleManualScreen}
-					/>
-					<Stack.Screen name="Schedule" component={ScheduleScreen} />
-
-					<Stack.Screen name="Profile" component={ProfileScreen} />
-
-					<Stack.Screen name="QuoteVehicle" component={ QuoteVehicleScreen } />
-					<Stack.Screen name="QuoteService" component={QuoteServiceScreen} />
-					<Stack.Screen name="QuoteReview" component={QuoteReviewScreen} />
-
-					{/* for test */}
-					<Stack.Screen name="test" component={TEST} />
-				</Stack.Navigator>
-			</NavigationContainer>
-		</ApolloProvider>
+		<Provider value={client}>
+			<View style={styles.container}>
+				<Input />
+				<View style={styles.responseView}>
+					<Messages />
+				</View>
+				<StatusBar style="auto" />
+			</View>
+		</Provider>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	responseView: {
+		flex: 2,
+	},
+	requestView: {
+		flex: 1,
+		marginTop: 400,
+	},
+
+	button: {
+		borderStyle: "solid",
+		borderWidth: 2,
+		marginTop: 10,
+	},
+
+	inputBox: {
+		borderStyle: "solid",
+		borderWidth: 2,
+	},
+});
