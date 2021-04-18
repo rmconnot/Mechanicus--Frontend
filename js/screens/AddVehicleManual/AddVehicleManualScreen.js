a68a563929e9bebaac5114498f53199b46328d7b
PeterZHAO98 <wenhuipeter98@gmail.com>
2021-04-18 09:43:05 +0800

    Update AddVehicleManualScreen.js

--- js/screens/AddVehicleManual/AddVehicleManualScreen.js
+++ js/screens/AddVehicleManual/AddVehicleManualScreen.js
@@ -1,31 +1,62 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { TextInput, Text, View, FlatList, TouchableOpacity, Alert, Image, Button } from 'react-native';
-import { CustomPicker } from '../../common/CustomPicker';
-import BottomNav from '../../common/BottomNav';
+import { gql, useMutation } from "@apollo/client";
 import styles from './Styles';
 
 
-export default class AddVehicleManualScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        // this.dataModel = getDataModel();
-        this.state = {
+const VEHICLE_MUTATION = gql`
+	mutation($customerID: Int!, $vin: String!, $type: String!, $year: Int!, $make: String!, $model: String!, $imgUrl: String!) {
+		createVehicle(customerID: $customerID, vin: $vin, vehicleType: $type, year: $year, make: $make, model: $model, imgUrl: $imgUrl) {
+			id
+		}
+	}
+`;
+
+export const AddVehicleManualScreen = ({ navigation }) => {
+    const [input, setInput] = useState({
+        customerID: 1,
         vin: "",
         type:"",
-        year:"",
-        maker: "",
+        year:0,
+        make: "",
         model: "",
-        };
-    }
+        imgUrl: "",
+	});
+
+    const [makeRequest, { data }, error] = useMutation(VEHICLE_MUTATION, {
+		onError: (error) => console.error(`Error! ${error}`),
+	});
+
     
-    onChange = (event, selectedDate) => {
-        const currentDate = selectedDate || date;
-        //setShow(Platform.OS === 'ios');
-        this.setState({date: currentDate});
-    }
+	const addVehicle = async () => {
+        makeRequest({
+            variables: {
+                customerID: input.customerID,
+                vin: input.vin,
+                type: input.type,
+                year: input.year,
+                make: input.make,
+                model: input.model,
+                imgUrl: input.imgUrl
+            },
+        }).then((result) => {
+            if (result.error) {
+                Alert.alert("VIN already in use", result.error.message, [
+                    { text: "OK", style: "OK" },
+                ]);
+            } else {
+                console.log(result);
+                navigation.navigate("VehicleList", {
+                    currentUser: {
+                        id: input.customerID,
+                    },
+                });
+                return;
+            }
+        });
+	};
     
-    render(){
-        return (
+    return (
         <View style={styles.container}>
             <Text style={styles.logo}>Add Vehicle Maunally</Text>
             <View style={styles.inputView} >
@@ -33,37 +64,52 @@ export default class AddVehicleManualScreen extends React.Component {
                 style={styles.inputText}
                 placeholder="Enter VIN of your vehicle" 
                 placeholderTextColor="#003f5c"
-                onChangeText={text => this.setState({vin:text})}/>
+                onChangeText={(text) =>
+                    setInput((prevState) => ({ ...prevState, vin: text.trim() }))
+                }
+            />
             </View>
             <View style={styles.inputView} >
             <TextInput  
                 style={styles.inputText}
                 placeholder="Enter type of your vehicle" 
                 placeholderTextColor="#003f5c"
-                onChangeText={text => this.setState({type:text})}/>
+                onChangeText={(text) =>
+                    setInput((prevState) => ({ ...prevState, type: text.trim() }))
+                }                
+            />
             </View>
             <View style={styles.inputView} >
             <TextInput  
                 style={styles.inputText}
                 placeholder="Enter year of your vehicle" 
                 placeholderTextColor="#003f5c"
-                onChangeText={text => this.setState({year:text})}/>
+                onChangeText={(text) =>
+                    setInput((prevState) => ({ ...prevState, year: parseInt(text.trim(),10) }))
+                }
+            />
             </View>
             <View style={styles.inputView} >
             <TextInput  
                 style={styles.inputText}
                 placeholder="Enter maker of your vehicle" 
                 placeholderTextColor="#003f5c"
-                onChangeText={text => this.setState({maker:text})}/>
+                onChangeText={(text) =>
+                    setInput((prevState) => ({ ...prevState, make: text.trim() }))
+                }
+            />            
             </View>
             <View style={styles.inputView} >
             <TextInput  
                 style={styles.inputText}
                 placeholder="Enter model of your vehicle" 
                 placeholderTextColor="#003f5c"
-                onChangeText={text => this.setState({model:text})}/>
+                onChangeText={(text) =>
+                    setInput((prevState) => ({ ...prevState, model: text.trim() }))
+                }
+            />                
             </View>
-            <TouchableOpacity style={styles.registerBtn} onPress={() => Alert.alert('submit request & jump to the vehicle page'+this.state)}>
+            <TouchableOpacity style={styles.registerBtn} onPress={addVehicle}>
             <Text style={styles.registerText}>Confirm</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.cancelBtn} onPress={() => Alert.alert('jump to the quote page')}>
@@ -72,5 +118,4 @@ export default class AddVehicleManualScreen extends React.Component {
     
         </View>
         );
-    }
 }
