e7ae6f5b211ed26b6722928a179ee50e601efdca
Yuxuan Li <54911077+victoriali8023@users.noreply.github.com>
2021-04-26 19:43:08 -0400

    idk changes

--- test1Screen.js
+++ test1Screen.js
@@ -1,78 +1,38 @@
 import React, { useState } from "react";
-import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import { View, Text, Button, StyleSheet, TextInput} from 'react-native';
 import BottomNav, { NavGroup } from './js/common/BottomNav';
 import TopNav from './js/common/TopNav';
 import {CheckboxItem, ServiceCheckbox, CheckboxGroup} from './js/common/Form';
+import {Picker} from '@react-native-community/picker'
 
-
-const sampleServiceList = [
-	{
-		id: "01",
-		type: "Vehicle Inspection",
-		price: 120,
-	},
-	{
-		id: "02",
-		type: "Oil change",
-		price: 100,
-        options: [
-			{
-				id: "02-1",
-				type: "synthetic oil",
-				price: 65,
-			},
-			{
-				id: "02-2",
-				type: "synthetic blends",
-				price: 70,
-			},
-			{
-				id: "02-3",
-				type: "high mileage oil",
-				price: 100,
-			},
-			{
-				id: "02-4",
-				type: "conventional oil",
-				price: 120,
-			},
-			
-		]
-	},
-	{
-		id: "03",
-		type: "Brake repair",
-		price: 90,
-	},
-	{
-		id: "04",
-		type: "Battery replacement",
-		price: 70,
-	},
-	{
-		id: "05",
-		type: "Battery Jump Service",
-		price: 20,
-	},
-];
 
 /* <ProfileScreen> */
 export function test1Screen({navigation}) {
 
-    const [servicesListProp, setServicesListProp] = useState();
-
-    const handleCheckedServices = (servicesList) => {
-		
-		setServicesListProp(servicesList);
-	};
-
+	const [selectedValue, setSelectedValue] = useState("11:00-11:15am");
+	const currentDate = new Date();
+	const currentHour = currentDate.getHours();
+	const currentDay = currentDate.getDay();
+	
     return (
-        <View style={styles.container}> 
-            <TopNav navigation={navigation} activated = "Appointments"/>
-            <CheckboxGroup
-					handleCheckedServices={handleCheckedServices}
-				/>
-        </View>
+		<View style={styles.container}>
+		<Picker
+			mode="dropdown"
+			selectedValue={selectedValue}
+			style={{ height: 50, width: 300 }}
+			onValueChange={(value, itemIndex) => setSelectedValue(value)}
+		>
+		  <Picker.Item label="11:00-12:00pm" value="11:00-12:00pm"/>
+		  <Picker.Item label="12:00-1:00pm" value="12:00-1:00pm"/>
+		  <Picker.Item label="1:00-2:00pm" value="1:00-2:00pm"/>
+		  <Picker.Item label="2:00-3:00pm" value="2:00-3:00pm"/>
+		  <Picker.Item label="3:00-4:00pm" value="3:00-4:00pm"/>
+		  <Picker.Item label="4:00-5:00pm" value="4:00-5:00pm"/>
+		  <Picker.Item label="5:00-6:00pm" value="5:00-6:00pm"/>
+		  <Picker.Item label="6:00-6:30pm" value="6:00-6:30pm"/>
+		</Picker>
+		{console.log(selectedValue)}
+	  </View>
         
     );
 } 
