3ccddbba033638e167ae27df59938516455c277c
Robert Connot <rmconnot@umich.edu>
2021-05-14 16:11:09 -0400

    disclaimer added to Profile screen

--- js/screens/Profile/ProfileScreen.js
+++ js/screens/Profile/ProfileScreen.js
@@ -1,72 +1,77 @@
-import * as React from 'react';
-import { View, Text, Button, StyleSheet, SafeAreaView } from 'react-native';
-import BottomNav from '../../common/BottomNav';
+import * as React from "react";
+import { View, Text, Button, StyleSheet, SafeAreaView } from "react-native";
+import BottomNav from "../../common/BottomNav";
 import { gql, useQuery } from "@apollo/client";
 import styles from "./Styles";
-import {Icon} from '../../common/Svg'
-import { colors, commonStyles } from '../../common/Style';
+import { Icon } from "../../common/Svg";
+import { colors, commonStyles } from "../../common/Style";
 
 const CUSTOMER_QUERY = gql`
 	query($id: Int!) {
 		customerProfile(id: $id) {
 			firstName
-            lastName
-            zipcode
-            streetAddress1
-            streetAddress2
+			lastName
+			zipcode
+			streetAddress1
+			streetAddress2
 		}
 	}
 `;
 
 /* <ProfileScreen> */
 export function ProfileScreen({ navigation, route }) {
-    
-    const { currentUser } = route.params;
-    console.log(route.params);
+	const { currentUser } = route.params;
+	console.log(route.params);
 
-    const {data, loading, error} = useQuery(CUSTOMER_QUERY,{
-        variables: {
-            id: currentUser.id   //use currentUser.id when get previous route
-        },
-    });
+	const { data, loading, error } = useQuery(CUSTOMER_QUERY, {
+		variables: {
+			id: currentUser.id, //use currentUser.id when get previous route
+		},
+	});
 
-    if (loading) return (<Text>Loading...</Text>);
-    if (error) return (<Text>Oh no... {error.message}</Text>);
+	if (loading) return <Text>Loading...</Text>;
+	if (error) return <Text>Oh no... {error.message}</Text>;
 
-    return (
-        <SafeAreaView style={commonStyles.container}> 
-            <View style={[commonStyles.pageContainer, {flex:1}]}>
-                <View style={styles.titleRow}>
-                    <Icon name = "account" color = {colors.gray4} size = {56}/>
-                    <Text style={[commonStyles.h2, styles.userName]}>{data.customerProfile.firstName} {data.customerProfile.lastName}</Text>
-                </View>
-                
-                <View style={styles.group}>
-                    <Text style={[commonStyles.body,styles.label]}>info</Text>
-                    
-                    <View style={styles.inputBox}>
-                        <Text style={commonStyles.h3}>Zipcode</Text>
-                        {/* <Text style={styles.inputTextContent}>{data.customerProfile.zipcode}</Text> */}
-                    </View>
-                    <View style={styles.inputBox}>
-                        <Text style={commonStyles.h3}>Addresses</Text>
-                        {/* <Text>{data.customerProfile.streetAddress2} {data.customerProfile.streetAddress1}</Text> */}
-                    </View>
-                </View>
+	return (
+		<SafeAreaView style={commonStyles.container}>
+			<View style={[commonStyles.pageContainer, { flex: 1 }]}>
+				<Text style={styles.warningText}>Page under construction</Text>
+				<View style={styles.titleRow}>
+					<Icon name="account" color={colors.gray4} size={56} />
+					<Text style={[commonStyles.h2, styles.userName]}>
+						{data.customerProfile.firstName} {data.customerProfile.lastName}
+					</Text>
+				</View>
 
-                <View style={styles.group}>
-                    <Text style={[commonStyles.body,styles.label]}>Settings</Text>
+				<View style={styles.group}>
+					<Text style={[commonStyles.body, styles.label]}>info</Text>
 
-                    <View style={styles.inputBox}>
-                        <Text style={commonStyles.h3}>Notification</Text>
-                    </View>
-                    <View style={styles.inputBox}>
-                        <Text style={commonStyles.h3}>About Mechanicus</Text>
-                    </View>
-                </View>
+					<View style={styles.inputBox}>
+						<Text style={commonStyles.h3}>Zipcode</Text>
+						{/* <Text style={styles.inputTextContent}>{data.customerProfile.zipcode}</Text> */}
+					</View>
+					<View style={styles.inputBox}>
+						<Text style={commonStyles.h3}>Addresses</Text>
+						{/* <Text>{data.customerProfile.streetAddress2} {data.customerProfile.streetAddress1}</Text> */}
+					</View>
+				</View>
 
-            </View>
-            <BottomNav navigation={ navigation } routeProps={route} activated = "Account"/>
-        </SafeAreaView>
-    );
-} 
+				<View style={styles.group}>
+					<Text style={[commonStyles.body, styles.label]}>Settings</Text>
+
+					<View style={styles.inputBox}>
+						<Text style={commonStyles.h3}>Notification</Text>
+					</View>
+					<View style={styles.inputBox}>
+						<Text style={commonStyles.h3}>About Mechanicus</Text>
+					</View>
+				</View>
+			</View>
+			<BottomNav
+				navigation={navigation}
+				routeProps={route}
+				activated="Account"
+			/>
+		</SafeAreaView>
+	);
+}
--- js/screens/Profile/Styles.js
+++ js/screens/Profile/Styles.js
@@ -1,54 +1,57 @@
-import { StyleSheet, Dimensions } from 'react-native';
-import {fonts, colors, commonStyles} from "../../common/Style"
+import { StyleSheet, Dimensions } from "react-native";
+import { fonts, colors, commonStyles } from "../../common/Style";
 
 const styles = StyleSheet.create({
-  row: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  titleRow:{
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 80,
-    marginBottom: 24,
-  },
-  userName: {
-    marginTop:12,
-  },
-  profileIcon:{
-    marginTop:80,
-    marginLeft:"43%"
-  },
-  label: {
-    color: colors.gray2,
-    marginBottom: 8,
-  },
-  group: {
-    marginBottom: 32,
-  },
-  inputBox: {
+	row: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	titleRow: {
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 80,
+		marginBottom: 24,
+	},
+	userName: {
+		marginTop: 12,
+	},
+	profileIcon: {
+		marginTop: 80,
+		marginLeft: "43%",
+	},
+	label: {
+		color: colors.gray2,
+		marginBottom: 8,
+	},
+	group: {
+		marginBottom: 32,
+	},
+	inputBox: {
 		borderStyle: "solid",
-		backgroundColor: 'white',
-		padding:12,
+		backgroundColor: "white",
+		padding: 12,
 		marginBottom: 12,
 		borderRadius: 8,
-        flexDirection:'row'
+		flexDirection: "row",
 	},
-    inputTextTitle: {
+	inputTextTitle: {
 		fontSize: fonts.h3,
 		color: colors.text,
-        paddingLeft: 20
-	},
-    inputTextContent: {
 		paddingLeft: 20,
-        paddingTop: 5,
-		fontSize: fonts.note,
-		color: colors.text
 	},
-  bottom: {
-    paddingTop:110
-  }
+	inputTextContent: {
+		paddingLeft: 20,
+		paddingTop: 5,
+		fontSize: fonts.note,
+		color: colors.text,
+	},
+	bottom: {
+		paddingTop: 110,
+	},
+	warningText: {
+		color: "red",
+	},
 });
 
 export default styles;
